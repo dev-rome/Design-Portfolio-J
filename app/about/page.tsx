4c97697900b6c2f3943f12c2676c24fd22ba99d6
dev-rome <haynesjerome@gmail.com>
2024-02-29 16:37:22 -0500

    added about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
+import PersonalImage from "../../public/assets/images/test-person-img.jpg";
+
 export default function About() {
-  return <div>hello</div>;
+  return (
+    <section className="py:mt-16 col-span-4 py-10 md:col-start-3 md:col-end-13">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-2xl font-bold md:text-xl">I'm John Murillo</h1>
+
+        <p className="my-3 max-w-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis at
+          erat pellentesque adipiscing commodo elit.
+        </p>
+
+        <p className="font-bold">Contact me: test-email@gmail.com</p>
+
+        <Image
+          src={PersonalImage}
+          alt="John Murillo"
+          width={500}
+          height={500}
+          className="mt-5"
+        />
+      </div>
+    </section>
+  );
 }
