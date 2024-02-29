e3e599f4f22193f2eb79ad5959c20f14c1aa4273
dev-rome <haynesjerome@gmail.com>
2024-02-29 17:20:00 -0500

    removed gallery items

--- constants/galleryItems.ts
+++ constants/galleryItems.ts
@@ -1,38 +1,6 @@
 export const galleryItems = [
   {
     id: 1,
-    src: "/assets/images/actionvance-m2h5-H_08Qk-unsplash.jpg",
-    alt: "Test Title",
-    title: "Test Title",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    id: 2,
-    src: "/assets/images/coach.jpg",
-    alt: "Test Title",
-    title: "Test Title",
-    description:
-      "Lorem ipsum.",
-  },
-  {
-    id: 3,
-    src: "/assets/images/fashion.jpg",
-    alt: "Test Title",
-    title: "Test Title",
-    description:
-      "Lorem ipsum dolor sit amet.",
-  },
-  {
-    id: 4,
-    src: "/assets/images/green-phone.jpg",
-    alt: "Test Title",
-    title: "Test Title",
-    description:
-      "Lorem ipsum dolor.",
-  },
-  {
-    id: 5,
     src: "/assets/images/jordan.jpg",
     alt: "Test Title",
     title: "Test Title",
@@ -40,7 +8,7 @@ export const galleryItems = [
       "Lorem ipsum.",
   },
   {
-    id: 6,
+    id: 2,
     src: "/assets/images/nmbc.jpg",
     alt: "Test Title",
     title: "Test Title",
@@ -48,7 +16,7 @@ export const galleryItems = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
-    id: 7,
+    id: 3,
     src: "/assets/images/rose.jpg",
     alt: "Test Title",
     title: "Test Title",
@@ -56,7 +24,7 @@ export const galleryItems = [
       "Lorem ipsum.",
   },
   {
-    id: 8,
+    id: 4,
     src: "/assets/images/tablet.jpg",
     alt: "Test Title",
     title: "Test Title",
@@ -64,7 +32,7 @@ export const galleryItems = [
       "Lorem ipsum dolor.",
   },
   {
-    id: 9,
+    id: 5,
     src: "/assets/images/tech.jpg",
     alt: "Test Title",
     title: "Test Title",
