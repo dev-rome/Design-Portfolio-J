08e24c8edc2c8f059fce9aab67f36dc222d92a20
dev-rome <haynesjerome@gmail.com>
2024-02-29 16:34:15 -0500

    added onClick to types

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,13 @@
 export interface NavbarProps {
   title: string;
   path: string;
+  onClick?: () => void;
 }
-
 export interface SocialLinkProps {
   title: string;
   path: string;
   Icon: React.ElementType;
+  onClick?: () => void;
 }
 export interface GalleryProps {
   src: string;
