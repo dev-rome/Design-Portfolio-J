7a7bc25c65ddd6c9fcdcfcaafa3f885c95b6eca1
dev-rome <haynesjerome@gmail.com>
2024-02-29 12:19:22 -0500

    add nav and social links

---
+++ constants/navLinks.ts
@@ -0,0 +1,5 @@
+export const navLinks = [
+  { title: "Work", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+];
---
+++ constants/socialLinks.ts
@@ -0,0 +1,7 @@
+import { FaBehance } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+
+export const socialLinks = [
+  { title: "Behance", path: "#", Icon: FaBehance },
+  { title: "Mail", path: "#", Icon: MdMail },
+];
