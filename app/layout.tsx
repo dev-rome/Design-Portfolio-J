ead2de1d893c3d82a3a15617331aea62127630c3
dev-rome <haynesjerome@gmail.com>
2024-02-29 17:41:40 -0500

    import back to top to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
+import BackToTopButton from "@/components/BackToTopButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             <Navbar />
             {children}
           </div>
+          <BackToTopButton />
         </main>
       </body>
     </html>
