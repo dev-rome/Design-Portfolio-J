dada279a8a61d37147533d3f0a81118cd1f1a0c1
dev-rome <haynesjerome@gmail.com>
2024-02-29 09:41:48 -0500

    project setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { MdMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <p>Hello</p>
+    </nav>
+  );
+}
