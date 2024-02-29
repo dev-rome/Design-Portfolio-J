53f79cf1c9a9ff062a7a10bac29a65fce448ea69
dev-rome <haynesjerome@gmail.com>
2024-02-29 15:48:13 -0500

    add galleryItems array to store image data for the gallery feature

---
+++ constants/galleryItems.ts
@@ -0,0 +1,74 @@
+export const galleryItems = [
+  {
+    id: 1,
+    src: "/assets/images/actionvance-m2h5-H_08Qk-unsplash.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    src: "/assets/images/coach.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum.",
+  },
+  {
+    id: 3,
+    src: "/assets/images/fashion.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 4,
+    src: "/assets/images/green-phone.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum dolor.",
+  },
+  {
+    id: 5,
+    src: "/assets/images/jordan.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum.",
+  },
+  {
+    id: 6,
+    src: "/assets/images/nmbc.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 7,
+    src: "/assets/images/rose.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum.",
+  },
+  {
+    id: 8,
+    src: "/assets/images/tablet.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum dolor.",
+  },
+  {
+    id: 9,
+    src: "/assets/images/tech.jpg",
+    alt: "Test Title",
+    title: "Test Title",
+    description:
+      "Lorem ipsum.",
+  },
+];
